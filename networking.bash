a9f28a7b0537666163c12b6c8980157c9102c893
Keane Wolter <daemoneye2@gmail.com>
2019-04-14 20:23:58 -0400

    List IPv4 and IPv6 addresses for all interfaces except loopback

--- networking.bash
+++ networking.bash
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-eth0IPv4=`/sbin/ip addr show eth0 | grep "inet\b" | awk {'print $2'} | cut -d'/' -f1`
-eth0IPv6=`/sbin/ip addr show eth0 | grep "inet6" | awk {'print $2'} | cut -d'/' -f1`
-wlan0IPv4=`/sbin/ip addr show wlan0 | grep "inet\b" | awk {'print $2'} | cut -d'/' -f1`
-wlan0IPv6=`/sbin/ip addr show wlan0 | grep "inet6" | awk {'print $2'} | cut -d'/' -f1`
-
-echo "eth0 IPv4 Address is: $eth0IPv4"
-echo "eth0 IPv6 Address is: $eth0IPv6"
-echo "wlan0 IPv4 Address is: $wlan0IPv4"
-echo "wlan0 IPv6 Address is: $wlan0IPv6"
+for each in `/sbin/ip -o link show | cut -d' ' -f2 | cut -d':' -f1 | grep -v lo`
+do
+	echo $each IPv4 Address is: `/sbin/ip addr show $each | grep "inet\b" | awk {'print $2'} | cut -d'/' -f1`
+	echo $each IPv6 Address is: `/sbin/ip addr show $each | grep "inet6" | awk {'print $2'} | cut -d'/' -f1`
+done
 
 echo ""
 
