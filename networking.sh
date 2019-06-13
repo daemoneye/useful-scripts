f918858a1b8fc442179b0eba3a8f87504e3fbab6
Keane Wolter <daemoneye@localhost.localdomain>
2019-06-12 22:34:19 -0400

    Cleaned up output to list each IP address on it's own line

--- networking.sh
+++ networking.sh
@@ -2,8 +2,16 @@
 
 for each in `/sbin/ip -o link show | cut -d' ' -f2 | cut -d':' -f1 | grep -v lo`
 do
-	echo $each IPv4 Address is: `/sbin/ip addr show $each | grep "inet\b" | awk {'print $2'} | cut -d'/' -f1`
-	echo $each IPv6 Address is: `/sbin/ip addr show $each | grep "inet6" | awk {'print $2'} | cut -d'/' -f1`
+	echo "$each IPv4 has the following addresses:"
+	for address in `/sbin/ip addr show $each | grep "inet\b" | awk {'print $2'} | cut -d'/' -f1`
+	do
+		echo "\t$address"
+	done
+	echo "$each IPv6 has the following addresses:"
+	for address in `/sbin/ip addr show $each | grep "inet6" | awk {'print $2'} | cut -d'/' -f1`
+	do
+		echo "\t$address"
+	done
 done
 
 echo ""
